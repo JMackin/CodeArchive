f77f748b2f4ea1b5739b9fe39e66d1923887d8d9
JMackin <jonnnmackin@gmail.com>
2018-02-08 13:56:26 -0800

    Added new python script for c232

--- Python/CS232-01-jlm1455.py
+++ Python/CS232-01-jlm1455.py
@@ -9,13 +9,13 @@
 #Side Effects: prints each number in the iteration
 
 def multiples_in_range(iter_val,begin_val,end_val):
+    
     num_vals = 0
     for i in range(begin_val+iter_val,end_val,iter_val):
         print(i)
         num_vals += 1
         
     return num_vals
-
 
 
 #Problem 2
@@ -50,10 +50,10 @@ def letter_grade(score):
 
 
 #Problem 3
-# 
-#Purpose:
-#Returns: 
-#Side Effects:
+# anytype -> anytype
+#Purpose: returns the value given with an addition of an amount depending on the data type of the passed value
+#Returns: the passed value incrimented
+#Side Effects: returns original value if passed value is not of type int,float,string, or bool
 
 def bump_it(val):
     
@@ -67,10 +67,66 @@ def bump_it(val):
     else:
        return val
 
-#Problem 4
-# 
-#Purpose:
-#Returns: 
-#Side Effects:
 
+#Problem 4
+# string -> string
+#Purpose: creates a "nickname" from the string passed
+#Returns: a modified string containing the nickname
+#Side Effects: converts string to list for easy manipulation then back to string to be returned
+
+def my_name(name):
+
+    name_list = []
+    nickname = ""
+    for i in name: name_list.append(i)
+    
+    name_list[0] = name_list[0].upper()
+    if len(name_list) >= 5:
+        name_list = name_list[0:5]
+        if name_list[4] == 'y':
+            del name_list[4]
+        else:
+            name_list.append('y')
+            
+    for i in name_list: nickname += str(i)
+    print(nickname)
+    return nickname
+
+
+#Problem 5
+# string -> string
+#Purpose: capitalizes or lowercases a string based on the case of itsa letters
+#Returns: a new string with modified cases
+#Side Effects: if the string is a combo of upper and lower case letters, the function will only flip the first letter.
+
+def flip_case(msg):
+    
+    if msg.isupper():
+        flipped_msg = msg.lower()
+    elif msg.islower():
+        flipped_msg = msg.upper()
+    else:
+        if msg[0].isupper():
+            flipped_msg = msg[0].lower() + msg[1:]
+        else:
+            flipped_msg = msg[0].upper() + msg[1:]
+
+
+    return flipped_msg
+
+
+#Problem 6
+# num -> list
+#Purpose: returns a list of numbers in the fibonnaci sequence of a given length starting at 0,1
+#Returns: a list with generated fibonacci sequence
+#Side Effects: the list must begin at 0 for elegance
+ 
+def fibonacci_list(num):
+    
+    fib_list = [0,1]
+
+    for i in range(2,num):
+        fib_list.append(fib_list[i-1]+fib_list[i-2])
+
+    return fib_list
 
